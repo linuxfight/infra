9f232830d93f059b5ff33bb8de2620d42e60f134
linuxfight <kiko13024@gmail.com>
2026-03-09 03:44:59 +0300

    feat: add dns for wakatime

--- dns/main.tf
+++ dns/main.tf
@@ -19,6 +19,15 @@ resource "cloudflare_dns_record" "vault" {
   proxied = false
 }
 
+resource "cloudflare_dns_record" "waka" {
+  zone_id = var.zone_id
+  name    = "waka"
+  content = "85.208.85.194"
+  type    = "A"
+  ttl = 3600
+  proxied = false
+}
+
 resource "cloudflare_dns_record" "muse" {
   zone_id = var.zone_id
   name    = "muse"
--- gateway/nginx/scripts/certs.sh
+++ gateway/nginx/scripts/certs.sh
@@ -5,3 +5,4 @@ set -e
 sudo ./bin/lego --email "hello@lxft.tech" --domains="cloud.lxft.tech" --http run
 sudo ./bin/lego --email "hello@lxft.tech" --domains="vault.lxft.tech" --http run
 sudo ./bin/lego --email "hello@lxft.tech" --domains="muse.lxft.tech" --http run
+sudo ./bin/lego --email "hello@lxft.tech" --domains="waka.lxft.tech" --http run
