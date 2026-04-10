ceceabc049035c76f4fea6450f3f436ac00e172e
linuxfight <kiko13024@gmail.com>
2026-04-10 05:34:52 +0300

    fix: sync script

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
-rsync -avz --delete server/. main@192.168.1.152:/home/main/config/
+rsync -avz --delete --exclude "*.enc" server/. main@192.168.1.152:/home/main/config/
 rsync -avz --delete --exclude "*.enc" gateway/. main@cloud.lxft.dev:/home/main/config
