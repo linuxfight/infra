513c9ff933f18e0bd1659a41f1c6a1ec5d362bd6
linuxfight <kiko13024@gmail.com>
2026-05-08 18:25:29 +0300

    feat: add wakapi sso, add navidrome

--- dns/main.tf
+++ dns/main.tf
@@ -63,3 +63,12 @@ resource "cloudflare_dns_record" "feed" {
   ttl = 3600
   proxied = false
 }
+
+resource "cloudflare_dns_record" "music" {
+  zone_id = var.zone_id
+  name    = "music"
+  content = "85.208.85.194"
+  type    = "A"
+  ttl = 3600
+  proxied = false
+}
