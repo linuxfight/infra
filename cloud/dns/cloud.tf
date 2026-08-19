4847a897ad3d9e2998e8e968204615dd6ae8b19e
linuxfight <kiko13024@gmail.com>
2026-08-20 00:37:05 +0300

    feat(cloud): split DNS based on host

--- cloud/dns/cloud.tf
+++ cloud/dns/cloud.tf
@@ -1,34 +1,16 @@
-resource "cloudflare_dns_record" "de1" {
+resource "cloudflare_dns_record" "cloud" {
   zone_id = var.cloudflare_zone_id
-  name    = "de1"
-  content = "149.33.40.182"
+  name    = "cloud"
+  content = "85.208.85.194"
   type    = "A"
   ttl = 3600
   proxied = false
 }
 
-resource "cloudflare_dns_record" "ru1" {
+resource "cloudflare_dns_record" "id_staging" {
   zone_id = var.cloudflare_zone_id
-  name    = "ru1"
-  content = "201.24.118.2"
-  type    = "A"
-  ttl = 3600
-  proxied = false
-}
-
-resource "cloudflare_dns_record" "www" {
-  zone_id = var.cloudflare_zone_id
-  name    = "www"
-  content = "lxft.dev"
-  type    = "CNAME"
-  ttl = 3600
-  proxied = false
-}
-
-resource "cloudflare_dns_record" "root" {
-  zone_id = var.cloudflare_zone_id
-  name    = "@"
-  content = "ru1.lxft.dev"
+  name    = "id.staging"
+  content = "cloud.lxft.dev"
   type    = "CNAME"
   ttl = 3600
   proxied = false
