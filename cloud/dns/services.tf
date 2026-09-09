772976507b44e9710ee2486770a3ab4fc0662622
linuxfight <kiko13024@gmail.com>
2026-09-09 13:20:34 +0300

    terraform: add dns record for authelia

--- cloud/dns/services.tf
+++ cloud/dns/services.tf
@@ -16,6 +16,15 @@ resource "cloudflare_dns_record" "www" {
   proxied = false
 }
 
+resource "cloudflare_dns_record" "authelia" {
+  zone_id = var.cloudflare_zone_id
+  name    = "id"
+  content = "cloud.lxft.dev"
+  type    = "CNAME"
+  ttl = 3600
+  proxied = false
+}
+
 resource "cloudflare_dns_record" "forgejo" {
   zone_id = var.cloudflare_zone_id
   name    = "git"
